78da668ff5781c12d843f333a7d060bffb054ad6
Saba125 <jiadzeb@gmail.com>
2024-09-04 00:32:55 +0400

    first commit

---
+++ src/_root/pages/Home.tsx
@@ -0,0 +1,33 @@
+import Loader from "@/components/shared/Loader";
+import PostCard from "@/components/shared/PostCard";
+import { useGetRecentPost } from "@/lib/react-query/queriesAndMutations";
+import { Models } from "appwrite";
+import React from "react";
+
+const Home = () => {
+  const {
+    data: posts,
+    isLoading: isPostLoading,
+    isError: isErrorPosts,
+  } = useGetRecentPost();
+  return (
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          {isPostLoading && !posts ? (
+            <Loader />
+          ) : (
+            <ul>
+              {posts?.documents.map((post: Models.Document) => (
+                <PostCard post={post} />
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
